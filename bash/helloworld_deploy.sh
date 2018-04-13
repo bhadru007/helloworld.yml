945c2eb06dd49d236fd7f95a0462f2a60c9db38e
John Boynton <jcboynton76@yahoo.com>
2018-04-13 13:01:10 -0400

    Testing

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -4,15 +4,16 @@ MY_PATH="`dirname \"$0\"`"
 
 
 
-mkdir $MY_PATH/buildDir/package -p
+mkdir $MY_PATH/buildDir -p
 
 cd $MY_PATH/buildDir
 
 git clone https://github.com/warpedcodemonkey/helloworld.git
 
-zip -r helloworld.zip helloworld
+cd helloworld
+zip -r helloworld.zip **.*
 
-aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source $MY_PATH/buildDir/
+aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source $MY_PATH/buildDir/helloworld
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
 
