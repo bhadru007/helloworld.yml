e4a528dd1179c9692666b014b79c30d21ea0abf9
John Boynton <jcboynton76@yahoo.com>
2018-04-13 13:26:23 -0400

    Finally got the paths figured out

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -13,7 +13,7 @@ git clone https://github.com/warpedcodemonkey/helloworld.git
 cd helloworld
 zip -r helloworld.zip **.*
 
-aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source ./buildDir/helloworld
+aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source .
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
 
