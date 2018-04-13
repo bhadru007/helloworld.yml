508c4325e38aec1ed2e05eab3abbdb5fed93716d
John Boynton <jcboynton76@yahoo.com>
2018-04-13 12:44:41 -0400

    Changed bash from /tmp, ansible does not like the path

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-mkdir /tmp/buildDir/package -p
+MY_PATH="`dirname \"$0\"`"
 
-cd /tmp/buildDir
+
+
+mkdir $MY_PATH/buildDir/package -p
+
+cd $MY_PATH/buildDir
 
 git clone https://github.com/warpedcodemonkey/helloworld.git
 
 zip -r ./package/helloworld.zip helloworld
 
-aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source /tmp/buildDir/package/
+aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source $MY_PATH/buildDir/package/
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
 
 cd ~
 
-rm -fr /tmp/buildDir
+rm -fr $MY_PATH/buildDir
