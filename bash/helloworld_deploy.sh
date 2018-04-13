c5667dd797a36b10b9230b1786023d8573bbc130
John Boynton <jcboynton76@yahoo.com>
2018-04-13 12:04:12 -0400

    Creating zip bundle

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-mkdir /tmp/buildDir
+mkdir /tmp/buildDir/package -p
 
 cd /tmp/buildDir
 
 git clone https://github.com/warpedcodemonkey/helloworld.git
 
-aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source /tmp/buildDir/helloworld/
+zip -r ./package/helloworld.zip helloworld
+
+aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source /tmp/buildDir/package/
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
 
