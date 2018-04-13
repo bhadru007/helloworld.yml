939c3a1463bb1e5b4e4c84cc7913e0713f63b5cb
John Boynton <jcboynton76@yahoo.com>
2018-04-13 13:15:21 -0400

    Bash path corrections, chasing ghost here

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -4,18 +4,18 @@ MY_PATH="`dirname \"$0\"`"
 
 
 
-mkdir $MY_PATH/buildDir -p
+mkdir ./buildDir -p
 
-cd $MY_PATH/buildDir
+cd ./buildDir
 
 git clone https://github.com/warpedcodemonkey/helloworld.git
 
 cd helloworld
 zip -r helloworld.zip **.*
 
-aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source $MY_PATH/buildDir/helloworld
+aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source ./buildDir/helloworld
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
 
 
-rm -fr $MY_PATH/buildDir
+rm -fr ./buildDir
