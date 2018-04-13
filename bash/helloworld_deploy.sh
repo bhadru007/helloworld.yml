66ed54a6b090305e7648eedb0dee98a1f84a53bc
John Boynton <jcboynton76@yahoo.com>
2018-04-13 18:13:03 -0400

    Added wait to deploy

--- bash/helloworld_deploy.sh
+++ bash/helloworld_deploy.sh
@@ -13,6 +13,8 @@ git clone https://github.com/warpedcodemonkey/helloworld.git
 cd helloworld
 zip -r helloworld.zip **.*
 
+aws cloudformation wait stack-create-complete --stack-name "$2"
+
 aws deploy push --application-name $2 --description "A deployment" --ignore-hidden-files --s3-location s3://$1/deploy-bundle.zip --source .
 
 aws deploy create-deployment --application-name $2 --s3-location bucket=$1,key=deploy-bundle.zip,bundleType=zip --deployment-group-name $3 --deployment-config-name CodeDeployDefault.OneAtATime --description "My Deployment"
